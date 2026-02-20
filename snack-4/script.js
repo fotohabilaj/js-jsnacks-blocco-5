5766f814341db64097f55af230b696b37d182a6e
fotohabilaj <fotohabilaj@gmail.com>
2026-02-20 14:04:44 +0100

    snack 4 complete

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,7 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const autori = [];
+posts.forEach(autore => autori.push(autore.author));
+console.log(autori);
