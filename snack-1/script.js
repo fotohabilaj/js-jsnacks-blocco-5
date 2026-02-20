780bc4a624a639e1189adb572adc819b806f4d9b
fotohabilaj <fotohabilaj@gmail.com>
2026-02-20 13:57:34 +0100

    first commit

---
+++ snack-1/script.js
@@ -0,0 +1,6 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+let lista = [];
+names.forEach(element => console.log(element));
